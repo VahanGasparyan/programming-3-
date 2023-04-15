0655ad076bff4eda0515fc3b143b4a004e1dd55a
VahanGasparyan <vahan.gasparyan.g@tumo.org>
2023-04-15 12:44:48 +0400

    Start server

--- gameOfLife.Verjnakan/server.js
+++ gameOfLife.Verjnakan/server.js
@@ -1,1 +1,15 @@
-var expresss = require
+var express = require('express')
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var fs = require('fs')
+
+app.use(express.static("."));
+
+app.get('/',function (req,res){
+    res.redirect('index.html');
+});
+
+server.listen(3000,function(){
+    console.log("server is run");
+});
